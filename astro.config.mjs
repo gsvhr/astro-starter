e37f377830f3dc430282a33beef167a4deb5211a
Степан <gsv.tlt@gmail.com>
2026-01-14 17:44:09 +0400

    good

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx()],
   site: 'https://sudexpert-tlt.ru',
   trailingSlash: 'ignore',
 });
